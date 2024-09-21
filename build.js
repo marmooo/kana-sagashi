b26ef2e0abf7acf732b4cf7ba151c9ba282675a4
marmooo <marmooo@users.noreply.github.com>
2024-09-21 21:04:44 +0900

    use TextLineStream

--- build.js
+++ build.js
@@ -1,4 +1,4 @@
-import { readLines } from "https://deno.land/std/io/mod.ts";
+import { TextLineStream } from "jsr:@std/streams/text-line-stream";
 
 function isIdiom(word) {
   if (3 <= word.length && word.length <= 6 && /^[ァ-ヶ]+$/.test(word)) {
@@ -10,10 +10,11 @@ function isIdiom(word) {
 
 async function build(threshold) {
   const idioms = [];
-  const fileReader = await Deno.open(
-    `graded-vocab-ja/dist/0.csv`,
-  );
-  for await (const line of readLines(fileReader)) {
+  const file = await Deno.open("graded-vocab-ja/dist/0.csv");
+  const lineStream = file.readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextLineStream());
+  for await (const line of lineStream) {
     const arr = line.split(",");
     const word = arr[0];
     const count = parseInt(arr[1]);
